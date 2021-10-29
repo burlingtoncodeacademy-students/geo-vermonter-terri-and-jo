5af0a72508e6d173409fa1dfb1e2107311e21d4f
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-10-29 17:01:33 -0400

    Added comments to App.js for Story 2 (still working on Story 2)

--- src/App.js
+++ src/App.js
@@ -8,46 +8,50 @@ import Buttons from "./components/Buttons.js";
 function App() {
   //Sets the state for the map to be viewed on the coordinates in the center of the map
   const [center, setCenter] = useState([43.88, -72.7317]);
-  //Sets the state to handle random position of marker
-  const [marker, setMarker] = useState([{lat: 43.88, long: -72.7317}]);
+  //Sets the state to handle random position of marker on the map
+  const [marker, setMarker] = useState([{ lat: 43.88, long: -72.7317 }]);
 
-  //Function to sets a random location
+  //Function to set a random location on map
   function randomLocation(min, max) {
-    //Setting the range to be the difference between min and max plus 1 because the difference is one less than actual possibilities
+    //Sets the range to difference between min and max plus 1 - the difference is one less than actual possibilities so you add the +1
     let range = max - min + 1;
+    //Returns the floor of random number in the range set above
     return Math.floor(Math.random() * range) + min;
 
     // let longRandom = longMax - longMin + 1;
     // return Math.floor(Math.random() * longRandom) + longMin;
-
-    //use the setMarker method to set the state of our marker state with this random position
   }
+  //Function to set a random latitude
   function randomLat() {
+    //Returns a random location within the min and max latitudes of VT
     return randomLocation(42.730315, 45.005419);
   }
 
+  //Function to set a random longitude
   function randomLong() {
+    //Returns a random location within the min and max longitudes of VT
     return randomLocation(-73.35218, -71.510255);
   }
 
-  if(marker) {
+  //A guard clause to prevent infinite loop when calling the randomLong() and randomLat()
+  if (marker) {
+    //Intermediate variables setting above functions to lat and long respectively
     let lat = randomLat();
     let long = randomLong();
-    
-    setMarker({lat: lat, long: long});
 
-    //set initial state to these random functions either wrap in guard clause or initial state be set to these values 
+    //The setMarker method sets the state of our marker state with random lat and long position
+    setMarker({ lat: lat, long: long });
   }
 
-  //returns components
+  //Returns components
   return (
     <div id="mainContainer">
-      {/* pass marker position to map as a prop */}
       <Map center={center} marker={marker} />
+      {/* Center position and Marker position get passed to map as a props in Map component */}
       <Buttons />
     </div>
   );
 }
 
-//exports the App to index.js
+//Exports the App to index.js
 export default App;
